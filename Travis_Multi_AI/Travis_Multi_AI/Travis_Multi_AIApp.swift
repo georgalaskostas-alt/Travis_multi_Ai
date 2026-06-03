c12bac989d4ceffa9afe63845de3eb7f8572fccc
konstantinos <georgalas.kostas@gmail.com>
2026-06-03 09:55:08 +0300

    Remove nested git and import project files

---
+++ Travis_Multi_AI/Travis_Multi_AI/Travis_Multi_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travis_Multi_AIApp.swift
+//  Travis_Multi_AI
+//
+//  Created by Κωνσταντινος Γεωργαλας on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Travis_Multi_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
